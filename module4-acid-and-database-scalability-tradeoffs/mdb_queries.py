2ad3edb815d3c4ba7cf66c5ca1594634efdd7c59
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-02-04 21:54:58 -0800

    Completed MongoDB RPG queries

--- module4-acid-and-database-scalability-tradeoffs/mdb_queries.py
+++ module4-acid-and-database-scalability-tradeoffs/mdb_queries.py
@@ -12,10 +12,69 @@ dbname = getenv('DBNAME')
 
 client = pymongo.MongoClient(f"mongodb+srv://nsriniva:{passwd}@cluster0.vggnf.mongodb.net/{dbname}?retryWrites=true&w=majority")
 
-print(client)
+#print(client)
 db = client.rpg
 
 char_coll = db.characters
 
-print(char_coll.count_documents({}))
+total_chars = char_coll.count_documents({})
+
+print(f'{total_chars = }')
+
+
+total_items = db.armory_item.count_documents({})
+
+print(f'{total_items = }')
+
+total_weapons = db.armory_weapon.count_documents({})
+
+print(f'{total_weapons = }')
+
+
+total_non_weapons = total_items - total_weapons
+
+print(f'{total_non_weapons = }')
+items_per_char = list(char_coll.find({},{'name':1, 'num_items': 1, '_id':0}).limit(20))
+
+print(f'{items_per_char = }')
+
+weapons_per_char = list(char_coll.find({},{'name':1, 'num_weapons': 1, '_id':0}).limit(20))
+
+print(f'{weapons_per_char = }')
+
+# $group
+#{
+#  _id: 0,
+#  avg_item: {
+#    $avg: "$num_items"
+#  }
+#}
+
+avg_items = list(
+    char_coll.aggregate( 
+    [{ 
+    "$group" :  
+        {"_id" : 1,  
+         "avg_items" : {
+             "$avg" : "$num_items"
+             } 
+         }} 
+    ]) 
+)[0]['avg_items']
+
+print(f'{avg_items = }')
+
+avg_weapons = list(
+    char_coll.aggregate( 
+    [{ 
+    "$group" :  
+        {"_id" : 1,  
+         "avg_weapons" : {
+             "$avg" : "$num_weapons"
+             } 
+         }} 
+    ]) 
+)[0]['avg_weapons']
+
+print(f'{avg_weapons = }')
 
